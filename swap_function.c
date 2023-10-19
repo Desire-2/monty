4327f0f03f65121f2471b097617a08c8dd95f266
Desire-2 <bikorimanadesire5@gmail.com>
2023-10-19 03:20:22 -0700

    Opcode swap swaps the top two elements of the stack.

---
+++ swap_function.c
@@ -0,0 +1,31 @@
+#include "monty.h"
+/**
+ * _funcSwap - Function adds the top two elements of the stack.
+ * @head: Head stack
+ * @counter: line_number
+ * Return: no return
+*/
+void _funcSwap(stack_t **head, unsigned int counter)
+{
+    int _leng = 0, _aux;
+	stack_t *_head;
+
+	_head = *head;
+	while (-head)
+	{
+		_head = _head->next;
+		_leng++;
+	}
+	if (_leng < 2)
+	{
+		fprintf(stderr, "L%d: can't swap, stack too short\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		_free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	_head = *head;
+	_aux = _head->n;
+	_head->n = _head->next->n;
+	_head->next->n = _aux;
+}
